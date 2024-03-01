b0b15db1e73857cb9404a57d3388b8662dce2c60
Vlad Svyrydenko <skvyrl19@gmail.com>
2024-03-01 18:36:07 +0200

    cpu usage for process, handler if miner process crashed

--- pages/dashboards/local.py
+++ pages/dashboards/local.py
@@ -2,7 +2,7 @@ import os
 import re
 
 import psutil
-from PyQt5.QtCore import QProcess, QProcessEnvironment, QDateTime, QUrl
+from PyQt5.QtCore import QProcess, QProcessEnvironment, QDateTime, QUrl, QThread, pyqtSignal
 from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtWidgets import QMessageBox
 from datetime import datetime
@@ -13,9 +13,27 @@ from .base import DashboardPageBase
 import bittensor as bt
 
 
+class CpuUsageThread(QThread):
+    cpu_usage_signal = pyqtSignal(str)
+
+    def __init__(self, process_id):
+        super().__init__()
+        self.process_id = process_id
+
+    def run(self):
+        proc = psutil.Process(self.process_id)
+        try:
+            cpu_usage = f"{proc.cpu_percent(interval=1):.1f}%"
+        except psutil.NoSuchProcess:
+            print("Process not found")
+            cpu_usage = "0.0%"
+        self.cpu_usage_signal.emit(cpu_usage)
+
+
 class LocalDashboardPage(DashboardPageBase):
     def __init__(self, parent):
         super().__init__(parent)
+        self.cpu_usage_thread = None
 
     def handle_registration(self):
         self.output_area.append(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - You are not registered')
@@ -43,13 +61,17 @@ class LocalDashboardPage(DashboardPageBase):
 
     def stop_mining(self):
         if self.mining_process is not None and self.mining_process.state() == QProcess.Running:
-            self.mining_process.terminate()
             self.timer.stop()
+            self.mining_process.terminate()
             self.data_logger.info(f' Activity: Mining Time - {self.elapsed_time}')
             self.mining_process.waitForFinished()
             self.mining_process = None
-            self.mine_button.setText("Start Mining")
-            self.output_area.append(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - Stop Mining')
+
+    def on_mining_finished(self):
+        self.timer.stop()
+        self.output_area.append(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - Stop Mining')
+        self.mine_button.setText("Start Mining")
+        self.mining_process = None
 
     def is_running(self):
         return self.mining_process is not None and self.mining_process.state() == QProcess.Running
@@ -103,7 +125,7 @@ class LocalDashboardPage(DashboardPageBase):
         self.mining_process.start(command, args)
         if self.charts_group.isVisible():
             self.toggle_view()
-        self.mining_process.finished.connect(self.stop_mining)
+        self.mining_process.finished.connect(self.on_mining_finished)
         self.mine_button.setText("Stop Mining")
 
     def register_on_subnet(self):
@@ -167,20 +189,18 @@ class LocalDashboardPage(DashboardPageBase):
             minutes = (self.elapsed_time % 3600) // 60
             seconds = self.elapsed_time % 60
             self.timer_label.setText(f"{hours}h: {minutes}m: {seconds}s")
-            # print(self.timer_label.text())
-            self.cpu_usage_label.setText(self.get_cpu_usage())
+            self.update_cpu_usage()
             self.gpu_usage_label.setText(self.get_gpu_usage())
 
-    @staticmethod
-    def get_cpu_usage():
-        """
-        Fetches the current CPU usage percentage.
+    def update_cpu_usage(self):
+        if self.cpu_usage_thread is None or not self.cpu_usage_thread.isRunning():
+            self.cpu_usage_thread = CpuUsageThread(self.mining_process.pid())
+            self.cpu_usage_thread.cpu_usage_signal.connect(self.update_cpu_usage_label)
+            self.cpu_usage_thread.start()
 
-        Returns:
-            str: CPU usage percentage as a string with one decimal place.
-        """
-        cpu_usage = psutil.cpu_percent()  # interval=1 provides a 1-second average
-        return f"{cpu_usage:.1f}%"
+    def update_cpu_usage_label(self, cpu_usage):
+        # Update the CPU usage label with the value received from the thread
+        self.cpu_usage_label.setText(cpu_usage)
 
     @staticmethod
     def get_gpu_usage():
